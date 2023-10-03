84fedf635d3e88b2614624b4dc44223e21930a1e
Adam Cahill <20097733@mail.wit.ie>
2023-10-03 12:04:04 +0100

    Completed Week 1 Lab

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -4,13 +4,11 @@ import Task from './components/Task';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" />
-      <Task title="Laundry" deadline="Tomorrow">
-          Fold laundry and put away
-      </Task>
-      <Task title="Tidy" deadline="Today" />
+      <Task title="Dishes" deadline="Today" description="Wash the dishes in the sink"/>
+      <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away"/>
+      <Task title="Tidy" deadline="Today" description="Tidy all the bedrooms"/>
     </div>
   );
 }
